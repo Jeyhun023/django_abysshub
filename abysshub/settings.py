d43ae346495307c772e5b56957e8f46f512a4ad4
Jeyhun Rashidov <creshidov23@gmail.com>
2022-03-16 16:53:23 +0400

    fixed cors

--- abysshub/settings.py
+++ abysshub/settings.py
@@ -25,8 +25,8 @@ SECRET_KEY = 'django-insecure-^6lj49)=ct^s^g1@o7$yk9g7&6v(mcn19%v0zd*ggy*5fp9dp9
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django.abysshub.com','www.django.abysshub.com','3.128.171.56', '127.0.0.1']
-
+ALLOWED_HOSTS=['*']
+CORS_ORIGIN_ALLOW_ALL = True
 
 # Application definition
 
@@ -35,6 +35,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'rest_framework',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
@@ -45,6 +46,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware'
 ]
 
 ROOT_URLCONF = 'abysshub.urls'
